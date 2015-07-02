2a1f60b9cd68cb7545a1669a625ade3b51588f95
Yilin Mo <ylmo@ntu.edu.sg>
2015-07-02 16:02:44 +0800

    restructure

--- org-publish.el
+++ org-publish.el
@@ -11,6 +11,15 @@
 	 :headline-levels 4             ; Just the default for this project.
          :body-only t ;; Only export section between <body> </body>
 	 )
+	("org-posts"
+	 :base-directory "~/homepage/org/post/"
+	 :base-extension "org"
+	 :publishing-directory "~/homepage/_posts"
+	 :recursive t
+	 :publishing-function org-html-publish-to-html
+	 :headline-levels 4             ; Just the default for this project.
+         :body-only t ;; Only export section between <body> </body>
+	 )
 	("org-static"
 	 :base-directory "~/homepage/"
 	 :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|pdf"
