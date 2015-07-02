141fdaca8ec4b328e0ccd3a2e7a4f56b58051e11
yilinmo <moyilin1@gmail.com>
2015-07-02 11:31:06 +0800

    save

--- org-publish.el
+++ org-publish.el
@@ -2,21 +2,21 @@
 
 (setq org-publish-project-alist
       '(
-	("org-notes"
+	("org-pages"
 	 :base-directory "~/homepage/"
 	 :base-extension "org"
 	 :publishing-directory "~/homepage/"
 	 :recursive t
 	 :publishing-function org-html-publish-to-html
 	 :headline-levels 4             ; Just the default for this project.
-	 :auto-preamble t
+         :body-only t ;; Only export section between <body> </body>
 	 )
 	("org-static"
 	 :base-directory "~/homepage/"
-	 :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+	 :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|pdf"
 	 :publishing-directory "~/homepage/"
 	 :recursive t
 	 :publishing-function org-publish-attachment
 	 )
-	("org" :components ("org-notes" "org-static"))
+	("org" :components ("org-pages" "org-static"))
 	))
